1fc34cad8bb97fff141b49b361551d54068ab355
adilsonbarbosajpg <apina1@byupathway.edu>
2026-09-04 23:10:44 -0300

    asas

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Dynamically output current year
+    const currentYearSpan = document.getElementById("currentyear");
+    if (currentYearSpan) {
+        currentYearSpan.textContent = new Date().getFullYear();
+    }
+
+    // Dynamically output last modified date
+    const lastModifiedParagraph = document.getElementById("lastModified");
+    if (lastModifiedParagraph) {
+        lastModifiedParagraph.textContent = `Last Modification: ${document.lastModified}`;
+    }
+});
